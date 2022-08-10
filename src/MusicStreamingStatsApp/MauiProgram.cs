f9d1c740d6c757eaa9ba46acbc199f26f068fb88
Michal Diviš <michall.divis@gmail.com>
2022-08-10 09:20:35 +0200

    added a lottie animation for people listening now

--- src/MusicStreamingStatsApp/MauiProgram.cs
+++ src/MusicStreamingStatsApp/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace MusicStreamingStatsApp;
+﻿using SkiaSharp.Views.Maui.Controls.Hosting;
+
+namespace MusicStreamingStatsApp;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseSkiaSharp()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
