f84e27ae64e5acdb3b8e0aef8d55640f24cda0be
Michal Diviš <michall.divis@gmail.com>
2022-08-10 10:05:11 +0200

    added font icons

--- src/MusicStreamingStatsApp/MauiProgram.cs
+++ src/MusicStreamingStatsApp/MauiProgram.cs
@@ -14,7 +14,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("fa-solid-900.ttf", "FaSolid");
+            });
 
 		return builder.Build();
 	}
